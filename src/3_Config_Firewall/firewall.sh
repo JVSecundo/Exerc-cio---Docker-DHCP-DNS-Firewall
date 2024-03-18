a470a0e0254f2f800eac8db32a4cf0576898c297
JVSecundo <joaovictorsecundo402@gmail.com>
2024-03-18 00:16:52 -0300

    finalizado

---
+++ src/3_Config_Firewall/firewall.sh
@@ -0,0 +1,28 @@
+
+tail -f /dev/null
+#Limpando todas as regras existentes
+
+iptables -F
+iptables -X
+
+#definindo a política padrão como DROP (bloquear tudo)
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+#permitindo conexões de loopback
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+#Permitindo tráfego relacionado e estabelecido
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+#permitindo tráfego DHCP
+iptables -A INPUT -p udp --dport 67:68 --sport 67:68 -j ACCEPT
+
+#permitindo tráfego DNS
+iptables -A INPUT -p udp --dport 53 -j ACCEPT
+iptables -A INPUT -p tcp --dport 53 -j ACCEPT
+
+#bloqueado o trem 
+iptables -A INPUT -s 200.18.168.100 -p icmp --icmp-type echo-request -j DROP
